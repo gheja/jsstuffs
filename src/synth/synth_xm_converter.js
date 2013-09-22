4988479de18e240cd1e1a915b60389652437ca6c
Gabor Heja <gheja@cubeone>
2013-09-22 21:21:25 +0000

    synth: created SynthXmConverter.readFile()

--- src/synth/synth_xm_converter.js
+++ src/synth/synth_xm_converter.js
@@ -1,4 +1,14 @@
 SynthXmConverter = function()
 {
+	this.log = function(s)
+	{
+		console.log("SynthXmConverter: " + s);
+	}
 	
+	this.readFile = function(data)
+	{
+		var xm_reader = new SynthXmReader();
+		
+		return xm_reader.readFile(data);
+	}
 }
