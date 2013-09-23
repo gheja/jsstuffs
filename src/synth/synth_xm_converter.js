d246d03a03ef09c180f3898ffe728470cea3e304
Gabor Heja <gheja@cubeone>
2013-09-23 18:52:56 +0000

    SynthXmConverter: modified processFiles() to perform a column compression on each channels.

--- src/synth/synth_xm_converter.js
+++ src/synth/synth_xm_converter.js
@@ -36,6 +36,7 @@ SynthXmConverter = function()
 		var i, j, k, l, m,
 			found = 0,
 			index = -1,
+			channels = 0,
 			pattern_column_dictionary = [],
 			pattern_column_map = [],
 			pattern,
@@ -43,7 +44,7 @@ SynthXmConverter = function()
 		
 		this.log("Processing " + this.xm_structures.length + " songs...");
 		
-		this.log("Creating the pattern column dictionary and map...");
+		this.log("Creating the pattern-channel column dictionary and map...");
 		for (i in this.xm_structures)
 		{
 			// read all the patterns into the merged_pattern_table
@@ -51,58 +52,62 @@ SynthXmConverter = function()
 			{
 				pattern_column_map[j] = [];
 				pattern = this.xm_structures[i].patterns[j];
+				channels = this.xm_structures[i].header.number_of_channels;
 				
-				for (k=0; k<5; k++)
+				for (n=0; n<channels; n++)
 				{
-					pattern_column = [];
-					for (l=0; l<pattern.number_of_rows; l++)
+					for (k=0; k<5; k++)
 					{
-						pattern_column.push(pattern.data_unpacked[l][k]);
-					}
-					
-					index = pattern_column_dictionary.length;
-					
-					// check if we already stored an equivalent column array
-					for (l=0; l<pattern_column_dictionary.length; l++)
-					{
-						if (pattern_column_dictionary[l].length == pattern_column.length)
+						pattern_column = [];
+						for (l=n; l<pattern.number_of_rows; l += channels)
 						{
-							found = 1;
-							for (m=0; m<pattern_column.length; m++)
+							pattern_column.push(pattern.data_unpacked[l][k]);
+						}
+						
+						index = pattern_column_dictionary.length;
+						
+						// check if we already stored an equivalent column array
+						for (l=0; l<pattern_column_dictionary.length; l++)
+						{
+							if (pattern_column_dictionary[l].length == pattern_column.length)
 							{
-								if (pattern_column_dictionary[l][m] != pattern_column[m])
+								found = 1;
+								for (m=0; m<pattern_column.length; m++)
 								{
-									found = 0;
+									if (pattern_column_dictionary[l][m] != pattern_column[m])
+									{
+										found = 0;
+										break;
+									}
+								}
+								if (found)
+								{
+									index = l;
 									break;
 								}
 							}
-							if (found)
-							{
-								index = l;
-								break;
-							}
 						}
+						
+						if (!found)
+						{
+							pattern_column_dictionary[index] = pattern_column;
+							this.log("    song #" + i + ", pattern #" + j + ", channel #" + n + ", column #" + k + " stored as #" + index);
+						}
+						else
+						{
+							this.log("    song #" + i + ", pattern #" + j + ", channel #" + n + ", column #" + k + " stored as #" + index + " (already stored)");
+						}
+						
+						pattern_column_map[j][k] = index;
+						
+						a++;
 					}
-					
-					if (!found)
-					{
-						pattern_column_dictionary[index] = pattern_column;
-						this.log("    song #" + i + ", pattern #" + j + ", column #" + k + " stored as #" + index);
-					}
-					else
-					{
-						this.log("    song #" + i + ", pattern #" + j + ", column #" + k + " stored as #" + index + " (already stored)");
-					}
-					
-					pattern_column_map[j][k] = index;
-					
-					a++;
 				}
 			}
 		}
 		this.log("Pattern column dictionary and map created.");
 		
-		this.log("total pattern columns: " + a + ", unique pattern columns: " + pattern_column_dictionary.length);
+		this.log("total pattern-channel columns: " + a + ", unique: " + pattern_column_dictionary.length);
 		
 		return true;
 	}
