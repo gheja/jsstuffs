1f91d10025b9554ed7beb7f4b7dc303b4b36413d
Gabor Heja <gheja@cubeone>
2013-09-23 17:52:51 +0000

    SynthXmConverter: added first part of processFiles().

--- src/synth/synth_xm_converter.js
+++ src/synth/synth_xm_converter.js
@@ -1,5 +1,7 @@
 SynthXmConverter = function()
 {
+	this.xm_structures = [];
+	
 	this.log = function(s)
 	{
 		console.log("SynthXmConverter: " + s);
@@ -23,6 +25,36 @@ SynthXmConverter = function()
 		}
 		
 		xm_reader.unpackPatternRows();
+		
+		this.xm_structures.push(xm_reader.getXmStructure());
+		
+		return true;
+	}
+	
+	this.processFiles = function()
+	{
+		var i, j, a, merged_pattern_table;
+		
+		this.log("Processing " + this.xm_structures.length + " songs...");
+		
+		merged_pattern_table = [];
+		k = 0;
+		this.log("Assembling the merged pattern table...");
+		for (i in this.xm_structures)
+		{
+			this.log("  start index of song #" + i + " in the merged pattern table is " + k);
+			
+			// read all the patterns into the merged_pattern_table
+			for (j=0; j<this.xm_structures[i].header.song_length; j++)
+			{
+				a = this.xm_structures[i].header.pattern_order_table[j];
+				this.log("    song #" + i + ", pattern order #" + j + " is pattern #" + a + " of song, copying it to the merged pattern table as #" + k);
+				
+				merged_pattern_table[k] = this.xm_structures[i].patterns[a].data_unpacked;
+				k++;
+			}
+		}
+		this.log("Merged pattern table assembled.");
 		
 		return true;
 	}
