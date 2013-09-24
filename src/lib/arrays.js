5135d7ed890516ef6c157c6b28467714c5f058c3
Gabor Heja <gheja@cubeone>
2013-09-24 21:50:23 +0000

    Added lib/arrays.js with ArbitaryArray.

---
+++ src/lib/arrays.js
@@ -0,0 +1,37 @@
+/** @constructor */
+ArbitaryArray = function(a, b)
+{
+	this.array = a ? a : [];
+	this.multiplier = b ? b : 256;
+	this.pointer = 0;
+	
+	this.add = function(value)
+	{
+		this.array.push(value);
+	}
+	
+	this.readOne = function()
+	{
+		return this.array[this.pointer++];
+	}
+	
+	this.readTwo = function()
+	{
+		return this.array[this.pointer++] + this.array[this.pointer++] * this.multiplier;
+	}
+	
+	this.eof = function()
+	{
+		return this.pointer >= this.array.length;
+	}
+	
+	this.getAsUint8Array = function()
+	{
+		return new Uint8Array(this.array);
+	}
+	
+	this.getAsUint16Array = function()
+	{
+		return new Uint16Array(this.array);
+	}
+}
