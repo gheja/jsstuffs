d794dfd972fad583e5b7804092462c878f038eae
Gabor Heja <gheja@cubeone>
2013-09-26 22:22:15 +0000

    lib/misc.js: added clamp_and_round_uint8(), clamp_and_round_int16() and build tests.

--- src/lib/misc.js
+++ src/lib/misc.js
@@ -2,3 +2,13 @@ clamp = function(a, b, c)
 {
 	return Math.min(Math.max(a, b), c);
 }
+
+clamp_and_round_uint8 = function(a)
+{
+	return clamp(a | 0, 0, 255);
+}
+
+clamp_and_round_int16 = function(a)
+{
+	return clamp(a | 0, -32768, 32767);
+}
--- test/lib_misc_test.js
+++ test/lib_misc_test.js
@@ -17,4 +17,32 @@ describe('Miscellaneous helpers', function() {
 			assert.equal(result, 20);
 		});
 	});
+	describe('clamp_and_round_uint8()', function() {
+		it('clamp_and_round_uint8(-2) should return 0', function () {
+			var result = clamp_and_round_uint8(-2);
+			assert.equal(result, 0);
+		});
+		it('clamp_and_round_uint8(123) should return 123', function () {
+			var result = clamp_and_round_uint8(123);
+			assert.equal(result, 123);
+		});
+		it('clamp_and_round_uint8(270) should return 255', function () {
+			var result = clamp_and_round_uint8(270);
+			assert.equal(result, 255);
+		});
+	});
+	describe('clamp_and_round_int16()', function() {
+		it('clamp_and_round_int16(-60000) should return -32768', function () {
+			var result = clamp_and_round_int16(-60000);
+			assert.equal(result, -32768);
+		});
+		it('clamp_and_round_int16(12345) should return 12345', function () {
+			var result = clamp_and_round_int16(12345);
+			assert.equal(result, 12345);
+		});
+		it('clamp_and_round_int16(70000) should return 32767', function () {
+			var result = clamp_and_round_int16(70000);
+			assert.equal(result, 32767);
+		});
+	});
 });
