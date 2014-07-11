be0689072bb385b74c7193abcfe19bc24396663c
Gabor Heja <gheja@cubeone>
2014-07-11 22:50:22 +0200

    lib/misc.js: renamed function parameters to match their purpose

--- src/lib/misc.js
+++ src/lib/misc.js
@@ -2,33 +2,33 @@
   * Clamps a given value between lower and upper limits.
   *
   * @nosideeffects
-  * @param {number} a the value to be clamped
-  * @param {number} b the lower limit
-  * @param {number} c the upper limit
+  * @param {number} value the value to be clamped
+  * @param {number} lower_limit the lower limit
+  * @param {number} upper_limit the upper limit
   */
-clamp = function(a, b, c)
+clamp = function(value, lower_limit, upper_limit)
 {
-	return Math.min(Math.max(a, b), c);
+	return Math.min(Math.max(value, lower_limit), upper_limit);
 }
 
 /**
   * Rounds then clamps a given value to fit in an unsigned 8 bit integer.
   *
   * @nosideeffects
-  * @param {number} a the value to be clamped
+  * @param {number} value the value to be clamped
   */
-clamp_and_round_uint8 = function(a)
+clamp_and_round_uint8 = function(value)
 {
-	return clamp(a | 0, 0, 255);
+	return clamp(value | 0, 0, 255);
 }
 
 /**
   * Rounds then clamps a given value to fit in a signed 16 bit integer.
   *
   * @nosideeffects
-  * @param {number} a the value to be clamped
+  * @param {number} value the value to be clamped
   */
-clamp_and_round_int16 = function(a)
+clamp_and_round_int16 = function(value)
 {
-	return clamp(a | 0, -32768, 32767);
+	return clamp(value | 0, -32768, 32767);
 }
