065ebe85ccce98a1d68161075226ae814b4106be
Gabor Heja <gheja@cubeone>
2014-08-29 19:40:36 +0200

    path_finder_pixels_demo: added interactive path-finding

--- src/path_finder_pixels_demo.js
+++ src/path_finder_pixels_demo.js
@@ -1,6 +1,8 @@
 var _pf = null;
 var _path = null;
 var _path2 = null;
+var _p_dest = [ 6, 14 ];
+var _refresh_in_progress = false;
 
 /*
 var _pixels = [
@@ -60,16 +62,19 @@ function draw_pf(canvas_name, mode)
 				ctx.fillRect(x * 16 + 1, y * 16 + 1, 15, 15);
 			}
 			
-			switch (_pf.nodes[x][y].status)
+			if (_pf.nodes.length > 0)
 			{
-				case 0:
-				break;
-				case 1:
-					ctx.fillStyle = "#0c9";
-				break;
-				case 2:
-					ctx.fillStyle = "#086";
-				break;
+				switch (_pf.nodes[x][y].status)
+				{
+					case 0:
+					break;
+					case 1:
+						ctx.fillStyle = "#0c9";
+					break;
+					case 2:
+						ctx.fillStyle = "#086";
+					break;
+				}
 			}
 			
 			ctx.fillRect(x * 16 + 4, y * 16 + 4, 9, 9);
@@ -93,7 +98,7 @@ function draw_pf(canvas_name, mode)
 		ctx.stroke();
 	}
 	
-	if (_path != null)
+	if (_path2 != null)
 	{
 		ctx.beginPath();
 		ctx.strokeStyle = "rgba(255, 255, 0, 0.5)";
@@ -111,9 +116,16 @@ function draw_pf(canvas_name, mode)
 	}
 }
 
-function init()
+function refresh()
 {
 	var i, squares;
+	
+	if (_refresh_in_progress)
+	{
+		return;
+	}
+	
+	_refresh_in_progress = true;
 	
 	var profiler = new Profiler();
 	
@@ -128,16 +140,46 @@ function init()
 	profiler.startInterval("generateRoute()");
 	//_path = _pf.generateRoute([ 0.1, 0.3 ], [ 8, 8 ]);
 	//_path = _pf.generateRoute([ 0.1, 0.3 ], [ 8, 9 ]);
-	_path = _pf.generateRoute([ 0.1, 0.3 ], [ 6, 14 ]);
+	_path = _pf.generateRoute([ 0.1, 0.3 ], _p_dest, 1000);
 	
-	profiler.startInterval("optimizePath()");
-	_path2 = _pf.optimizePath(_path);
+	if (_path)
+	{
+		profiler.startInterval("optimizePath()");
+		_path2 = _pf.optimizePath(_path);
+		_path2 = _pf.optimizePath(_path2);
+		_path2 = _pf.optimizePath(_path2);
+	}
+	else
+	{
+		_path2 = null;
+	}
 	
 	profiler.startInterval("drawing");
 	draw_pf("canvas1");
 	
 	profiler.finish();
 	profiler.dump();
+	
+	_refresh_in_progress = false;
+}
+
+function handle_mouse_move(e)
+{
+	var x, y;
+	
+	x = Math.min(16, Math.max(0, Math.round((e.clientX - e.originalTarget.offsetLeft - 8) / 16)));
+	y = Math.min(16, Math.max(0, Math.round((e.clientY - e.originalTarget.offsetTop - 8) / 16)));
+	
+	_p_dest = [ x, y ];
+	
+	refresh();
+}
+
+function init()
+{
+	canvas = document.getElementById("canvas1");
+	canvas.onmousemove = handle_mouse_move;
+	refresh();
 }
 
 window.onload = init;
