0435bc955bae983ee86f134264536fbf98b767d8
Gabor Heja <gheja@cubeone>
2014-08-31 21:09:11 +0200

    DisplayWebgl: added face normal calculation, added aVertexNormal and aNormalMatrix to shaders, added directional lighting to default shaders

--- src/display/display_webgl.js
+++ src/display/display_webgl.js
@@ -53,14 +53,18 @@ DisplayWebgl = function(parameters)
 		s.vertexColorAttribute = this.gl.getAttribLocation(s, "aVertexColor");
 		this.gl.enableVertexAttribArray(s.vertexColorAttribute);
 		
+		s.vertexNormalAttribute = this.gl.getAttribLocation(s, "aVertexNormal");
+		this.gl.enableVertexAttribArray(s.vertexNormalAttribute);
+		
 		s.modelViewProjectionMatrix = this.gl.getUniformLocation(s, "aModelViewProjectionMatrix");
+		s.normalMatrix = this.gl.getUniformLocation(s, "aNormalMatrix");
 		
 		return s;
 	}
 	
 	this.createBody = function(vertex_positions, vertex_colors)
 	{
-		var position_buffer, color_buffer, translated_vertex_positions, i;
+		var i, j, u, v, n, l, translated_vertex_positions, normals, position_buffer,  normal_buffer, color_buffer;
 		
 		// vertex Y and Z coordinates are swapped before rendering
 		// this way the XY plane becomes the ground
@@ -70,11 +74,48 @@ DisplayWebgl = function(parameters)
 			translated_vertex_positions.push(vertex_positions[i], vertex_positions[i+2], vertex_positions[i+1]);
 		}
 		
+		// calculate _face_ normals and attach them to the vertices
+		// TODO: optimize this
+		normals = [];
+		for (i=0; i<translated_vertex_positions.length; i+=9)
+		{
+			u = [
+				translated_vertex_positions[i+3+0] - translated_vertex_positions[i+0],
+				translated_vertex_positions[i+3+1] - translated_vertex_positions[i+1],
+				translated_vertex_positions[i+3+2] - translated_vertex_positions[i+2]
+			];
+			v = [
+				translated_vertex_positions[i+6+0] - translated_vertex_positions[i+0],
+				translated_vertex_positions[i+6+1] - translated_vertex_positions[i+1],
+				translated_vertex_positions[i+6+2] - translated_vertex_positions[i+2]
+			];
+			n = [
+				u[1] * v[2] - u[2] * v[1],
+				u[2] * v[0] - u[0] * v[2],
+				u[0] * v[1] - u[1] * v[0],
+			];
+			l = Math.sqrt(n[0]*n[0] + n[1]*n[1] + n[2]*n[2]);
+			n[0] /= l;
+			n[1] /= l;
+			n[2] /= l;
+			normals.push(
+				n[0], n[2], n[1],
+				n[0], n[2], n[1],
+				n[0], n[2], n[1]
+			);
+		}
+		
 		position_buffer = this.gl.createBuffer();
 		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, position_buffer);
 		this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(translated_vertex_positions), this.gl.STATIC_DRAW);
 		position_buffer.itemSize = 3;
 		position_buffer.numItems = translated_vertex_positions.length / 3;
+		
+		normal_buffer = this.gl.createBuffer();
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, normal_buffer);
+		this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(normals), this.gl.STATIC_DRAW);
+		normal_buffer.itemSize = 3;
+		normal_buffer.numItems = normals.length / 3;
 		
 		color_buffer = this.gl.createBuffer();
 		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, color_buffer);
@@ -84,6 +125,7 @@ DisplayWebgl = function(parameters)
 		
 		this.bodies.push({
 			positions: position_buffer,
+			normals: normal_buffer,
 			colors: color_buffer,
 			shader_program: this.default_shader_program
 		});
@@ -93,7 +135,7 @@ DisplayWebgl = function(parameters)
 	
 	this.renderObjects = function(view, projection, objects)
 	{
-		var i, o, model, mvp;
+		var i, o, model, mvp, normal;
 		
 		for (i=0; i<objects.length; i++)
 		{
@@ -113,6 +155,11 @@ DisplayWebgl = function(parameters)
 				mvp = Matrix.multiply(view, mvp);
 				mvp = Matrix.multiply(projection, mvp);
 				
+				normal = Matrix.identity();
+				// normal = Matrix.multiply(model, normal);
+				// normal = Matrix.multiply(view, normal);
+				normal = Matrix.inverse(normal);
+				
 				// TODO: alter the matrix to be transposed by default
 				mvp = Matrix.transpose(mvp);
 				
@@ -124,7 +171,11 @@ DisplayWebgl = function(parameters)
 				this.gl.bindBuffer(this.gl.ARRAY_BUFFER, o.colors);
 				this.gl.vertexAttribPointer(o.shader_program.vertexColorAttribute, o.colors.itemSize, this.gl.FLOAT, false, 0, 0);
 				
+				this.gl.bindBuffer(this.gl.ARRAY_BUFFER, o.normals);
+				this.gl.vertexAttribPointer(o.shader_program.vertexNormalAttribute, o.normals.itemSize, this.gl.FLOAT, false, 0, 0);
+				
 				this.gl.uniformMatrix4fv(o.shader_program.modelViewProjectionMatrix, false, mvp.m);
+				this.gl.uniformMatrix4fv(o.shader_program.normalMatrix, false, normal.m);
 				
 				this.gl.drawArrays(this.gl.TRIANGLES, 0, o.positions.numItems);
 			}
@@ -164,18 +215,25 @@ DisplayWebgl = function(parameters)
 	this.gl.clearColor(parameters.clear_color[0], parameters.clear_color[1], parameters.clear_color[2], 1.0);
 	this.gl.enable(this.gl.DEPTH_TEST);
 	
-	this.default_shader_program = this.createShaderProgram("\
-	attribute vec3 aVertexPosition; \
-	attribute vec4 aVertexColor; \
-	uniform mat4 aModelViewProjectionMatrix; \
-	varying vec4 vColor; \
-	void main(void) { \
-		gl_Position = aModelViewProjectionMatrix * vec4(aVertexPosition, 1.0); \
-		vColor = aVertexColor; \
-	}", "\
-	precision mediump float; \
-	varying vec4 vColor; \
-	void main(void) { \
-		gl_FragColor = vColor; \
-	}");
+	this.default_shader_program = this.createShaderProgram(" \
+		attribute vec3 aVertexPosition; \
+		attribute vec3 aVertexNormal; \
+		attribute vec4 aVertexColor; \
+		uniform mat4 aModelViewProjectionMatrix; \
+		uniform mat4 aNormalMatrix; \
+		varying vec4 vColor; \
+		void main(void) { \
+			gl_Position = aModelViewProjectionMatrix * vec4(aVertexPosition, 1.0); \
+			vec3 ambientColor = vec3(1.0, 1.0, 1.0); \
+			vec3 lightColor = vec3(1.0, 1.0, 1.0); \
+			vec3 lightDirection = vec3(1, 1, 1); \
+			vec4 transformedNormal = aNormalMatrix * vec4(aVertexNormal, 1.0); \
+			float lightValue = max(dot(transformedNormal.xyz, lightDirection), 0.0); \
+			vColor = aVertexColor * vec4(ambientColor * 0.4 + lightColor * lightValue * 0.6, 1.0); \
+		}", " \
+		precision mediump float; \
+		varying vec4 vColor; \
+		void main(void) { \
+			gl_FragColor = vColor; \
+		}");
 }
