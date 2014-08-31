343b3542a77a8d7a2c6326df836f1f015783cde3
Gabor Heja <gheja@cubeone>
2014-08-31 20:33:33 +0200

    DisplayWebgl: moved shaders to constructor

--- src/display_webgl_demo.js
+++ src/display_webgl_demo.js
@@ -1,23 +1,6 @@
 var _gl1 = null;
 var _camera = null;
 var _a = 0;
-var _vertex_shaders = [
-	"attribute vec3 aVertexPosition;\n" +
-	"attribute vec4 aVertexColor;\n" +
-	"uniform mat4 aModelViewProjectionMatrix;\n" +
-	"varying vec4 vColor;\n" +
-	"void main(void) {\n" +
-	"	gl_Position = aModelViewProjectionMatrix * vec4(aVertexPosition, 1.0);\n" +
-	"	vColor = aVertexColor;\n" +
-	"}"
-];
-var _fragment_shaders = [
-	"precision mediump float;\n" +
-	"varying vec4 vColor;\n" +
-	"void main(void) {\n" +
-	"	gl_FragColor = vColor;\n" +
-	"}\n"
-];
 var _objects = [];
 
 function init()
@@ -26,8 +9,6 @@ function init()
 	
 	_gl1 = new DisplayWebgl({
 		canvas_name: "canvas1",
-		vertex_shaders: _vertex_shaders,
-		fragment_shaders: _fragment_shaders,
 		clear_color: [ 0.1, 0.2, 0.3 ]
 	});
 	
